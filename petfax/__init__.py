38c10f8316c531322fbc091fe215ad792a725666
Christian Alquisada <calquisada@aol.com>
2024-02-17 14:19:42 -0800

    commit

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     # config
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Dilla702$@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
@@ -15,6 +23,7 @@ def create_app():
 
     # register fact blueprint
     from . import fact
-    app.register_blueprint(fact.bp)
+    app.register_blueprint(fact.bp)           
+
     
     return app
